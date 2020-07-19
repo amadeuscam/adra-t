d84b555c64071903c06f718aadbecb4a5db716c3
amadeuscam <amadeuscam@yahoo.es>
2020-07-19 08:55:30 +0200

    update celery config

--- adra_project/celery.py
+++ adra_project/celery.py
@@ -1,11 +1,11 @@
 import os
 from celery import Celery
 from celery.schedules import crontab
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adra_project.settings')
 
 app = Celery('adra_project')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object(settings, namespace='CELERY')
 app.autodiscover_tasks()
 app.conf.timezone = 'Europe/Madrid'
-
