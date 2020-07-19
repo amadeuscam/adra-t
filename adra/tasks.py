5498d898bc2502f9279617e2383aafb90d07cfa1
amadeuscam <amadeuscam@yahoo.es>
2020-07-19 09:01:51 +0200

    dsa

--- adra/tasks.py
+++ adra/tasks.py
@@ -8,11 +8,15 @@ from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 
 
+logger = get_task_logger(__name__)
+
+
 @periodic_task(
     run_every=(crontab(minute='1')),
     name="create_random_user_accounts",
     ignore_result=True
 )
 def create_random_user_accounts():
+    logger.info('Adding estoy corriendo!!!!')
     for i in range(1, 12):
         print(i)
