a015ac25c1882bb3cc91c1d32f6070610a1698b5
amadeuscam <amadeuscam@yahoo.es>
2020-07-31 08:14:24 +0200

    update

--- adra/forms.py
+++ adra/forms.py
@@ -1,15 +1,6 @@
 from django import forms
 from .models import Persona, Alimentos, Hijo,Profile
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-
-# class UserRegisterForm(UserCreationForm):
-#     email = forms.EmailField()
-
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password1', 'password2']
 
 
 class DateInput(forms.DateTimeInput):
--- adra/models.py
+++ adra/models.py
@@ -176,7 +176,7 @@ class AlmacenAlimentos(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='profile_pics',default='default.jpg', blank=True)
+    photo = models.ImageField(upload_to='profile_pics',default='default.png', blank=True)
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
