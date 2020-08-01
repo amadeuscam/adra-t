bf68b05b12a13dbd163a3978d796d860f636dd5e
amadeuscam <amadeuscam@yahoo.es>
2020-08-01 08:24:34 +0200

    Update send_email.py

--- adra/management/commands/send_email.py
+++ adra/management/commands/send_email.py
@@ -9,42 +9,4 @@ import sendgrid
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        try:
-            # Python 3
-            import urllib.request as urllib
-        except ImportError:
-            # Python 2
-            import urllib2 as urllib
-
-        sg = sendgrid.SendGridAPIClient(apikey="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")
-        data = {
-            "from": {
-                "email": "example@.sendgrid.net"
-            },
-            "personalizations": [
-                {
-                    "to": [
-                        {
-                            "email": "amadeuscam@yahoo.es"
-                        }
-                    ],
-                    "dynamic_template_data": {
-                        "url_cambiar": "https://adra-torrejon.herokuapp.com/",
-                        "Sender_Name": "Sample Name",
-
-                    }
-                }
-            ],
-            "template_id": "d-ab0adafe4dd14cb4b9aba688b7200830"
-        }
-        try:
-            response = sg.client.mail.send.post(request_body=data)
-        except urllib.HTTPError as e:
-            print(e.read())
-            exit()
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
-        # persona = Persona.objects.filter(nacionalidad__icontains="Marruecos")
-        # print(persona.count())
-        count = 0
+        pass
