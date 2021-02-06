3808b63e0b67a12b2ae9dad4b667abab0ee15a83
Lucian Cati <amadeuscam@yahoo.es>
2021-02-06 20:43:18 +0100

    Update tasks.py

--- adra/tasks.py
+++ adra/tasks.py
@@ -330,13 +330,9 @@ def make_backup_mysql():
     username = settings.USER_DB
     password = settings.PASSWORD_DB
     database = settings.NAME_DB
-    command_line = f"mysqldump -u {username} -p{password}  {database} > {datetime.now().day}-{datetime.now().month}.sql"
-    args = shlex.split(command_line)
-    subprocess.Popen(args)
 
-    # with open(f'{datetime.now().day}-{datetime.now().month}.sql', 'w') as output:
-    #     c = subprocess.Popen(['mysqldump', '-u', username, '-p%s' % password, database],
-    #                          stdout=output, shell=True)
+    command_line = f"mysqldump -u {username} -p{password}  {database} > {datetime.now().day}-{datetime.now().month}.sql"
+    os.popen(command_line)
 
 
 @shared_task
