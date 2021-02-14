5fa067a2951bdb654311ce188881563c09ec8468
Lucian Cati <amadeuscam@yahoo.es>
2021-02-14 20:28:22 +0100

    Update settings.py

--- adra_project/settings.py
+++ adra_project/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(env('SECRET_KEY'))
 
-Token_KEY_USER = str(env('TOKEN_USER'))
+Token_KEY_USER = str(env('Token_KEY_USER'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
