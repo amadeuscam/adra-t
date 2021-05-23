ace6518129d87ef93647b43386b1486db70f3354
Lucian Cati <amadeuscam@yahoo.es>
2021-05-23 20:38:58 +0200

    celery

--- adra_project/celery.py
+++ adra_project/celery.py
@@ -4,7 +4,7 @@ from celery.schedules import crontab
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adra_project.settings')
-# os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
+os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
 app = Celery('adra_project')
 app.config_from_object(settings, namespace='CELERY')
 app.autodiscover_tasks()
